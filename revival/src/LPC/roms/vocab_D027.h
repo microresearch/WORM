229eac843f53b5db3d4fff6752893a95ddb2da16
root <root@debian-BULLSEYE-live-builder-AMD64>
2024-03-12 20:23:01 +0100

    xx

--- revival/src/LPC/roms/vocab_D027.h
+++
@@ -1,3 +0,0 @@
-
-const uint8_t *wordlist_ffD027[13] = {sp_ffD027n0, sp_ffD027nn1, sp_ffD027nn2, sp_ffD027nn3, sp_ffD027nn4, sp_ffD027nn5, sp_ffD027nn6, sp_ffD027nn7, sp_ffD027nn8, sp_ffD027nn9, sp_ffD027nn10, sp_ffD027nn11};
-
--- revival/src/rsynth-2.0-port/phtoelm.h
+++
@@ -1,5 +0,0 @@
-/* phtoelm.h
-*/
-extern int phone_append (darray_ptr p,int ch);
-unsigned phone_to_elm (char *s, int n, darray_ptr elm);
-
